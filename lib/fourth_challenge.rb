1f70b8a84782fa21f59351bbf2a5c2dc48477caa
Shane Lonergan <sptlonergan@gmail.com>
2019-07-24 04:06:39 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -6,7 +6,7 @@ def fourth_challenge
       :hero => {name: "Romeo", age: "15", status: "alive"},
       :hero_friends => [
         {name: "Benvolio", age: "17", attitude:
-        "worried"}
+        "worried"},
         {name: "Mercutio", age: "18", attitude: "hot-headed"}
       ]
    },
@@ -15,7 +15,7 @@ def fourth_challenge
       :matriarch => {name: "Lady Capulet", age: "51"},
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => [
-        {name: "Steven", age: "30", attitude: "confused"}
+        {name: "Steven", age: "30", attitude: "confused"},
         {name: "Nurse", age: "44", attitude: "worried"}
       ]
    }
